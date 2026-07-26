2fc36909f5ea56f2b91694db53c1426ff59cac48
denisfernandogoncalves-cmd <denisfernando.goncalves@gmail.com>
2026-07-26 20:51:58 -0300

    feat: carregar administracao enterprise da producao

--- backend/apps/producao/apps.py
+++ backend/apps/producao/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProducaoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.producao"
+
+    def ready(self):
+        from . import grain_enterprise_admin  # noqa: F401
